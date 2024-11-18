affd2807d7780be1f70009ac156f2c11cacabfa6
Christoph Hornung <Gauss@BIGBROTHER>
2024-11-18 12:45:13 +0100

    Add project files.

---
+++ Migrations/20241118110903_NonTransactable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFGetStarted.Migrations
+{
+    /// <inheritdoc />
+    public partial class NonTransactable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NewPostId",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql("UPDATE Posts SET NewPostId = PostId+1", suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NewPostId",
+                table: "Posts");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class TestService
+{
+	private readonly BloggingContext db;
+
+	public TestService(BloggingContext db)
+	{
+		this.db = db;
+	}
+
+	public async Task ExecuteAsync()
+	{
+		if (File.Exists("blog.db"))
+		{
+			File.Delete("blog.db");
+		}
+
+		await this.db.Database.MigrateAsync();
+	}
+}
+
+public class Program
+{
+	private static IHost CreateHost() =>
+		Host.CreateDefaultBuilder()
+			.ConfigureServices((context, services) =>
+			{
+				services.AddDbContext<BloggingContext>(options =>
+					options.UseSqlite($"Data Source={"blog.db"}"));
+				services.AddScoped<TestService>();
+			})
+			.UseConsoleLifetime()
+			.Build();
+
+
+	private static async Task Main(string[] args)
+	{
+		IHost host = CreateHost();
+		await host.Services.GetRequiredService<TestService>().ExecuteAsync();
+
+		await host.RunAsync();
+	}
+}
